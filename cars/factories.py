fd1a9bcd9666a34fdfff13f096e981ca4602c826
Alex Penman-Hayes <alex.phayes@gmail.com>
2017-06-23 18:07:07 +0100

    Updating tests

--- cars/factories.py
+++ cars/factories.py
@@ -4,11 +4,11 @@ from .models import Car
 
 
 class CarFactory(factory.DjangoModelFactory):
-    make = factory.Iterator(Car.MAKES)
+    make = factory.Iterator(dict(Car.MAKES).values())
     manufacture_date = factory.LazyFunction(timezone.now)
-    colour = factory.Iterator(Car.COLOURS)
+    colour = factory.Iterator(dict(Car.COLOURS).values())
     sale_price = 999.99
-    fuel_efficiency_rating = factory.Iterator(Car.FUEL_EFFICIENCY)
+    fuel_efficiency_rating = factory.Iterator(dict(Car.FUEL_EFFICIENCY).values())
     has_ejector_seat = False
 
     class Meta:
--- cars/tests.py
+++ cars/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, RequestFactory
 from .models import Car
 from .factories import CarFactory
+import factory
 import json
 
 
@@ -24,3 +25,43 @@ class AddCarTestCase(TestCase):
     def test_add_car_invalid(self):
         response = self.client.post('/add-car/', data=json.dumps({}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+
+class EjectorCountTestCase(TestCase):
+    def setUp(self):
+        self.cars = factory.create_batch(CarFactory, 5, has_ejector_seat=True)
+
+    def tearDown(self):
+        for car in self.cars:
+            car.delete()
+
+    def test_ejector_count(self):
+        response = self.client.get('/ejector-count/')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertIn('count', data)
+        self.assertEqual(len(self.cars), data['count'])
+
+
+class MostCommonFuelRatingTestCase(TestCase):
+    def setUp(self):
+        self.cars = factory.create_batch(CarFactory, 5, has_ejector_seat=True)
+        self.control = CarFactory(fuel_efficiency_rating=Car.EFFICIENCY_C)
+
+    def tearDown(self):
+        for car in self.cars:
+            car.delete()
+        self.control.delete()
+
+    def test_ejector_count(self):
+        response = self.client.get('/most-common-fuel-rating/')
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertIn('fuel_efficiency_rating', data)
+        self.assertEqual(self.control.fuel_efficiency_rating, data['fuel_efficiency_rating'])
